5a1475a3e016521a8c83e3137d67c00cc72ad539
veronicarose27 <52283415+veronicarose27@users.noreply.github.com>
2019-07-16 22:54:37 +0530

    Update index03.py

--- index03.py
+++ index03.py
@@ -2,10 +2,10 @@ m=input()
 l=list(m)
 p=len(m)
 k=[]
+o=(p)//3
 if(p<=3):
     k.append(l[0])
 else:
-    k.append(l[0])
-    k.append(l[3])
+    for i in range(0,o+1):
+        k.append(l[i*3])
 print("".join(k))
-
